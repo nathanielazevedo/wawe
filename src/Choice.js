3179b51b12212d10b0e0c80032036f06401edb0c
nathanielazevedo <nathanielazevedo@hotmail.com>
2021-08-16 17:10:50 -0400

    image alt added

--- src/Choice.js
+++ src/Choice.js
@@ -5,7 +5,7 @@ import restaurant from "./restaurant.jpeg";
 function Choice() {
   return (
     <div className="choice">
-      <img src={restaurant} />
+      <img src={restaurant} alt="restaurant"/>
       <div className="footer">
         <h5 className="restaurant_title">New Holland Brewing</h5>
         <h5 className="restaurant_title">Grand Rapids, Michigan</h5>
